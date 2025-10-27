364db7a8336676df1dfeff2dfe21eb12e7572deb
sofiatoroviafara01 <sofiaviafara123@gmail.com>
2025-10-27 10:18:45 +0100

    add project files

---
+++ src/test/java/com/code_crafters/code_crafters/CodeCraftersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code_crafters.code_crafters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCraftersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
