85c887050e05e7afc40d911b88edc8390fa0a62c
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-10-30 12:41:56 +0100

    feat/(eventsmapper)implemented mapper

--- src/main/java/com/code_crafters/app/mapper/EventsMapper.java
+++ src/main/java/com/code_crafters/app/mapper/EventsMapper.java
@@ -1,5 +1,23 @@
 package com.code_crafters.app.mapper;
 
-public class EventsMapper {
-    
+import com.code_crafters.app.dto.request.EventsRequest;
+import com.code_crafters.app.dto.response.EventsResponse;
+import com.code_crafters.app.entity.Events;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface EventsMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creator", ignore = true)
+    @Mapping(target = "attendees", ignore = true)
+    Events toEntity(EventsRequest dto);
+
+    @Mapping(target = "userId", source = "creator.id")
+    @Mapping(target = "category", source = "category.name")
+    EventsResponse toDto(Events event);
+
+    void updateEntityFromDto(EventsRequest dto, @MappingTarget Events entity);
 }
