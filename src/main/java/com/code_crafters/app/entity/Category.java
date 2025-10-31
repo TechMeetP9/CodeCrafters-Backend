c105983fb8d5c55f1aea8e3e6a9b5854d5224767
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-10-31 09:33:05 +0100

    feat/(Entity)implememted location.java

--- src/main/java/com/code_crafters/app/entity/Category.java
+++ src/main/java/com/code_crafters/app/entity/Category.java
@@ -15,6 +15,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 15)
     private String name;
 }
+
