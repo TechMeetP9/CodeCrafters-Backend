18d845bd66c18f04199610bb38374d7bb0d3d2b3
Andrea <aoliverar.96@gmail.com>
2025-10-31 11:45:32 +0100

    feat:entity attendance done

--- src/main/java/com/code_crafters/app/entity/Attendance.java
+++ src/main/java/com/code_crafters/app/entity/Attendance.java
@@ -1,5 +1,47 @@
 package com.code_crafters.app.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "attendance")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Attendance {
-    
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Events event;
+
+    @Column(nullable = false)
+    private LocalDateTime joinedAt;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
